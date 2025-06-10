72c63661685fc052b25fba8af0f1123c2cf368b3
archive master <onlybussinessmail@gmail.com>
2025-06-10 22:16:54 +0900

    Player.cs

--- Player.cs
+++ Player.cs
@@ -2,25 +2,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+// 플레이어의 상호작용 및 상태를 관리하는 클래스
 public class Player : MonoBehaviour
 {
     //public TalkManager talkManager;
-    public GameObject scanObject;
-    public Button makebutton;
+    public GameObject scanObject; // 현재 상호작용 대상 오브젝트 (ex: NPC, 오브젝트 등)
+    public Button makebutton; // 아이템 제작 버튼
 
-    public ObjData currentQuest;
-    public GameObject inventoryCanvas;
+    public ObjData currentQuest; //현재 퀘스트 대상 오브젝트
+    public GameObject inventoryCanvas; // 인벤토리 UI 캔버스
 
-    bool isMaking = false;
+    bool isMaking = false; // 플레이어가 아이템 제작 중인지 여부
 
     void Awake()
     {
         // talkButton.onClick.AddListener(() => TalkStart());
         //talkManager = GameObject.FindWithTag("TalkManager").GetComponent<TalkManager>();
-        inventoryCanvas = GameObject.FindWithTag("InvenSingleTon");
+        inventoryCanvas = GameObject.FindWithTag("InvenSingleTon"); // 인벤토리 캔버스를 Tag로 찾아서 연결
     }
 
-    void FixedUpdate()
+    void FixedUpdate() // Ray를 통해 NPC감지 및 상호작용 처리 코드(비활성)
     {
         //RaycastHit rayHit;
 
@@ -42,12 +43,12 @@ public class Player : MonoBehaviour
         //}
     }
 
-    public void SetCurrentQuest(ObjData obj)
+    public void SetCurrentQuest(ObjData obj) //현재 퀘스트 오브젝트 설정
     {
         currentQuest = obj;
     }
 
-    public ObjData GetCurrentQuest()
+    public ObjData GetCurrentQuest() // 현재 퀘스트 오브젝트 반환
     {
         return currentQuest;
     }
@@ -60,46 +61,49 @@ public class Player : MonoBehaviour
     //    }
     //}
 
-    public void SetScanObject(GameObject npc)
+    public void SetScanObject(GameObject npc) //현재 상호작용 중인 오브젝트 설정
     {
         scanObject = npc;
     }
 
-    public GameObject GetScanObject()
+    public GameObject GetScanObject() //현재 상호작용 중인 오브젝트 반환
     {
         return scanObject;
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) //플레이어가 트리거 존에 진입했을 때 처리
     {
         if (other.CompareTag("inven"))
         {
-            InvenCanvas inventoryCanvasObj = inventoryCanvas.GetComponent<InvenCanvas>();
+        //인벤토리 UI 활성화
+            InvenCanvas inventoryCanvasObj = inventoryCanvas.GetComponent<InvenCanvas>(); 
             inventoryCanvasObj.SetActiveInven(true);
             isMaking = true;
         }
         else if(other.CompareTag("Portal"))
         {
+        //포탈 태그 충돌 시 씬 전환
             SceneManager.LoadScene("4_Forest");
         }
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider other) // 플레이어가 트리거 존을 벗어났을 때 처리
     {
         if (other.CompareTag("inven"))
         {
+        // 인벤토리 UI 비활성화
             InvenCanvas inventoryCanvasObj = inventoryCanvas.GetComponent<InvenCanvas>();
             inventoryCanvasObj.SetActiveInven(false);
             isMaking = false;
         }
     }
 
-    public void SetIsPlayerMaking(bool state)
+    public void SetIsPlayerMaking(bool state) // 제작 상태 설정
     {
         isMaking = state;
     }
 
-    public bool GetIsPlayerMaking()
+    public bool GetIsPlayerMaking() //제작 상태 여부 반환
     {
         return isMaking;
     }
